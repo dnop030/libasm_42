a4a8f6cfcc3be8693aaebe80cd54396de222777c
pass42 <dnop030@gmail.com>
2024-07-28 23:51:30 +0700

    ft_strlen.s -chg indent space to tabs

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -11,13 +11,13 @@ section .text
 global ft_strlen
 
 ft_strlen:
-    mov rax, 0              ; reset counter
+	mov rax, 0				; reset counter
 
-    jmp loop                ; whhile (*(s + counter) != 0)
-inc_cnt:                    ; {
-    inc rax                 ;   counter++
-loop:                       ;
-    cmp byte [rdi + rax], 0 ;
-    jne inc_cnt             ; }
+	jmp loop				; whhile (*(s + counter) != 0)
+inc_cnt:					; {
+	inc rax					;	counter++
+loop:						;
+	cmp byte [rdi + rax], 0	;
+	jne inc_cnt				; }
 
-    ret
+	ret
