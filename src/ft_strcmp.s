5c3184362070b989bf96aa6b9b9afb8f99f1dbf2
pass42 <dnop030@gmail.com>
2024-08-01 16:58:13 +0700

    ft_strcmp.s -add reg definition

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -8,6 +8,7 @@ section .text
 ;$rdi - ps1	(pointer to input string1)
 ;$rsi - ps2	(pointer to input string2)
 ;$rcx - cnt	(counter to increment pointer of both string)
+;$rdx - tmp reg to store *ps2 value in order to find diff
 ;$rax - diff(ruturn value of differential of string)
 
 global ft_strcmp
