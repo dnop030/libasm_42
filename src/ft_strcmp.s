533693a7f2ed37fb42c2292015f8f93a5da22ccc
pass42 <dnop030@gmail.com>
2024-08-01 16:55:28 +0700

    ft_strcmp.s -finish with code not clean

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -1,5 +1,5 @@
 ;ft_strcmp.s
-;int strcmp(const char *s1, const char *s2)
+;int ft_strcmp(const char *s1, const char *s2)
 
 
 section .text
@@ -12,4 +12,30 @@ section .text
 
 global ft_strcmp
 ft_strcmp:
+	mov	rcx, 0						; reset cnt
+	jmp	ifstr1Null
 
+inccnt:
+	inc rcx							; inc cnt
+ifstr1Null:
+	cmp	byte [rdi + rcx], 0				; if (*(ps1 + cnt) != NULL)
+	je	caloutput
+
+	; this line is not work due to cannot compare memory with memory
+	;cmp byte [rdi + rcx], [rsi + rcx]	; if *(ps1+cnt) == *(ps2+cnt)
+
+	mov	al, [rdi + rcx]
+	cmp byte al, [rsi + rcx]		; if (*(ps1 + cnt) == *(ps2 + cnt))
+	je	inccnt
+
+caloutput:
+	;; 1st version
+	;movzx	eax, byte [rdi + rcx]
+	;sub eax, byte [rsi + rcx]
+
+	; 2nd walk around => work!
+	movzx	eax, byte [rdi + rcx]
+	movzx	edx, byte [rsi + rcx]
+	sub	eax, edx
+
+	ret
