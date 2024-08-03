0f9bfe5369097c0febf04a8bae4f08c651d82ff1
pass42 <dnop030@gmail.com>
2024-08-03 10:10:39 +0700

    tutorial ex10.s -chg space to indent

--- tutorial/ex10.s
+++ tutorial/ex10.s
@@ -13,27 +13,27 @@ global _start
 extern __errno_location, printf
 
 _start:
-    ; Call a system function that sets errno (e.g., open a non-existing file)
-    ; Here we use an invalid system call for demonstration purposes
-    mov eax, 9999          ; Invalid syscall number
-    syscall                ; Trigger the syscall
+	; Call a system function that sets errno (e.g., open a non-existing file)
+	; Here we use an invalid system call for demonstration purposes
+	mov eax, 9999			; Invalid syscall number
+	syscall					; Trigger the syscall
 
-    ; Call __errno_location to get the address of errno
-    call __errno_location wrt ..plt
+	; Call __errno_location to get the address of errno
+	call __errno_location wrt ..plt
 
-    ; Store the pointer to errno in errnum
-    mov [errnum], rax
+	; Store the pointer to errno in errnum
+	mov [errnum], rax
 
-    ; Load the value of errno
-    mov rax, [rax]
+	; Load the value of errno
+	mov rax, [rax]
 
-    ; Print the errno value using printf
-    mov rsi, rax           ; Argument for printf (errno value)
-    lea rdi, [rel fmt]     ; Format string
-    xor eax, eax           ; Clear rax for variadic functions
-    call printf wrt ..plt
+	; Print the errno value using printf
+	mov rsi, rax			; Argument for printf (errno value)
+	lea rdi, [rel fmt]		; Format string
+	xor eax, eax			; Clear rax for variadic functions
+	call printf wrt ..plt
 
-    ; Exit the program
-    mov eax, 60            ; Syscall number for exit
-    xor edi, edi           ; Exit code 0
-    syscall
+	; Exit the program
+	mov eax, 60				; Syscall number for exit
+	xor edi, edi			; Exit code 0
+	syscall
