c19d6d5a1b07edaeab28857e02b56a18283d1b9b
pass42 <dnop030@gmail.com>
2024-08-04 01:17:35 +0700

    ft_write.s -add call __errno_location to handle error

--- src/ft_write.s
+++ src/ft_write.s
@@ -2,15 +2,27 @@
 
 section .text
 
+;Register usage:
+;$rax - tmp reg to keep return value from syscall and return to main Fn
+
 global ft_write
+
+extern __errno_location
+
 ft_write:
 	push rbp		;prologue
 	mov rbp, rsp	;prologue
 
-	mov rax, 1
+	mov rax, 1							; syscall write
 	syscall
-	cmp	rax, 0x00
+	cmp	rax, 0x00						; if (write < 0)
 	jg	return
+
+	; write error
+	push	rax							; keep status write to the stack
+	call	__errno_location wrt ..plt
+	pop		word [rax]
+	neg		word [rax]
 
 	mov	rax, -1
 
