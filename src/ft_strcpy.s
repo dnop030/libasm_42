5962167979e4a2ced891824ab44a95f49d9242b9
pass42 <dnop030@gmail.com>
2024-08-04 23:02:04 +0700

    ft_strcpy.s -fix bug wrong return value of function

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -5,11 +5,13 @@ section .text
 ;Register usage:
 ;$rdi - ps1	(pointer to input dst)
 ;$rsi - ps2	(pointer to input src)
-;$rax - pointer pointed at NULL of  dst
+;$rdx - tmp reg to cpy from src to dst due to cannot mov mem to mem
+;$rax - pointer pointed at dst
 
 global ft_strcpy
 
 ft_strcpy:
+	mov	rax, rdi			; prep return value
 	jmp	loop
 
 incptr:
@@ -17,13 +19,12 @@ incptr:
 	inc	rdi					; ps1++
 
 loop:
-	mov	rax, [rsi]			; cpy byte from src to dst
-	mov [rdi], rax			;
+	mov	rdx, [rsi]			; cpy byte from src to dst
+	mov [rdi], rdx			;
 	cmp	byte [rsi], 0x00	;
 	jne	incptr				; while (*ps1 != NULL)
 
 	inc rdi					; ps1++
 	mov byte [rdi], 0x00	; *ps1 = NULL
-	mov	rax, rdi			; prep return value
 
 	ret
