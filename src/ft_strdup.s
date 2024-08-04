7ca58e65260d5aa6f9e6082c7e08de1060b4f754
pass42 <dnop030@gmail.com>
2024-08-04 22:48:48 +0700

    ft_strdup.s -finish but found bug in ft_strcpy

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -3,26 +3,32 @@
 section .text
 
 ;Register usage:
+;r12 - ipptrstr keep ptr of input string
+;r13 - tmpptr keep ptr of new str
 
 global ft_strdup
 
 extern malloc, ft_strlen, ft_strcpy
 
 ft_strdup:
-	mov		r10, rdi			; keep ptr input str
+	push	r12
+	push	r13
+	mov		r12, rdi			; keep ptr input str
 	call	ft_strlen			; hope that rdi is pass to ft_strlen
 	inc		rax					; inc for NULL str at the end of dst
 	mov		rdi, rax			; assign strlen for malloc memory
 	call	malloc wrt ..plt
-	cmp		rax, 0x00			;
-	je		return
+	cmp		rax, 0x00			; if (malloc == NULL)
+	je		return				; return 0
 
-	mov		r11, rax			; keep ptr of new str from malloc
-	mov		rdi, r11			; assign dst pointer of str
-	mov		rsi, r10			; assign src pointer of str
+	mov		r13, rax			; keep ptr of new str from malloc
+	mov		rdi, r13			; assign dst pointer of str
+	mov		rsi, r12			; assign src pointer of str
 	call	ft_strcpy
-	mov		rax, r11			; prep return value
+	mov		rax, r13			; prep return value
 
 return:
 
+	pop		r13
+	pop		r12
 	ret
