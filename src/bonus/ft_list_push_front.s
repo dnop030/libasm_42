bcaf86218627d2a213978a9b91fd071f4d9d21a8
pass42 <dnop030@gmail.com>
2024-08-27 15:11:49 +0700

    ft_list_pust_front -start dev

--- src/bonus/ft_list_push_front.s
+++ src/bonus/ft_list_push_front.s
@@ -1,14 +1,34 @@
 ;void	ft_list_push_front(t_list **begin_list, void *data)
+;t_list	*ft_create_elem(void *data)
+
+section .bss
+
+struc t_list
+	data: resq	1
+	next: resq	1
+endstruc
 
 section .text
 
 ;Register usage:
-;rdi - **begin_list
-;rsi - *data
+;r12 - **begin_list
+;r13 - *data
 
 global ft_list_push_front
 
 extern	malloc
+extern	sizeof
+extern	__errno_location
 
 ft_list_push_front:
+	mov		r12, rdi
+	mov		r13, rsi
+	mov		rdi, r13
+	call	ft_create_elem
+
+;Register usage:
+;r12 - sizeof(t_list)
+ft_create_elem:
+	xor		rax				; reset output
+	mov		rdi,
 
