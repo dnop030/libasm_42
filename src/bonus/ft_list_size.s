a323015db25f39a0d9c954b180bb343ebe7124d1
Pratan Srikamonpattanawut <psrikamo@z3t8c1.42bangkok.com>
2024-08-28 01:24:49 +0700

    ft_list_size -finish but not clean code -work according to test of earth

--- src/bonus/ft_list_size.s
+++ src/bonus/ft_list_size.s
@@ -1,7 +1,31 @@
 ;int		ft_list_size(t_list *begin_list)
 
+section .bss
+
+struc t_list
+	.data: resq	1
+	.next: resq	1
+endstruc
+
+
 section .text
 
 global ft_list_size
 
+;Register usage:
+;rax - cnt => output
+;rdi - *begin_list
+
 ft_list_size:
+
+    xor     rax, rax
+loop:
+    cmp     qword rdi, 0
+    je      return
+    inc     rax
+    mov     qword rcx, [rdi + t_list.next]
+    mov     qword rdi, rcx
+    jmp     loop
+
+return:
+    ret
